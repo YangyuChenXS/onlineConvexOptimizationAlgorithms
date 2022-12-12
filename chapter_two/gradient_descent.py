7c73a107a01ac2c7de54512c87999d04f6bfd203
YangyuChenXS <chenxs12@outlook.com>
2022-12-12 15:05:41 +0800

    book

---
+++ chapter_two/gradient_descent.py
@@ -0,0 +1,8 @@
+def gradient_descent_fun(time_horizen, initial_x, step_sizes):
+    '''
+    最原始的梯度下降法 Example: f(x,y) = x^2 + y^2 +2
+    :param time_horizen: 迭代次数
+    :param initial_x:
+    :param step_sizes:
+    :return:
+    '''
