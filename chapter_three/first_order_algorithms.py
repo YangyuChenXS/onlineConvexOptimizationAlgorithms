eb08053651344ad3a41d58f4ff965fa49db482a3
YangyuChenXS <chenxs12@outlook.com>
2022-12-26 22:19:02 +0800

    proving_thm_three_two

--- chapter_three/first_order_algorithms.py
+++ chapter_three/first_order_algorithms.py
@@ -1,4 +1,9 @@
 import numpy
+import math
+import numpy
+import matplotlib.pyplot as plt
+
+# Chapter 3 Page-42
 def online_gradient_descent(time_horizon, initial_x):
     """
     与上一章的 basic gradient descent 最大的不同是每一轮的函数可以是不同的
@@ -24,3 +29,58 @@ def online_gradient_descent(time_horizon, initial_x):
             x_value_vector.append(x_value)
         # 这里不做投射->若需要投射，变成求范数最小值的优化问题即可
     return x_value_vector  # 理论上是不需要返回的，决策结束
+
+
+# Chapter 3 Page-42 Theorem 3.2
+def proving_thm_three_two(time_horizon):
+    """
+    # 验证Theorem 3.2最后一个不等式  C取1.6的时候最佳
+    :param time_horizon:
+    :return:
+    """
+    y_value = []
+    y_sqrt_value = []
+    x_value=[]
+    temp_one = 1
+    temp_two = 1
+    temp_three = 1
+    for t in range(2, time_horizon+2, 2):
+        x_value.append(t)
+        y_sqrt_value.append(math.sqrt(t))
+        for i in range(1, t):
+            temp_one = temp_one*i
+        for i in range(1, t//2+1):
+            temp_two = temp_two*i
+        for i in range(1, t//2):
+            temp_three = temp_three*i
+        y_value.append( t/(2**(t-2)) * (temp_one/temp_two/temp_three) )
+        temp_one = 1
+        temp_two = 1
+        temp_three = 1
+    #print(y_value)
+    #print(y_sqrt_value)
+    for i in range(len(y_sqrt_value)):
+        y_sqrt_value[i] = y_sqrt_value[i]*1.6  # 下界C值
+
+    difference_value = [] # sqrt_value 与 y_value的差值
+    for i in range(len(y_sqrt_value)):
+        difference_value.append(y_sqrt_value[i]-y_value[i])
+    #print(difference_value)
+
+    fig, axes = plt.subplots(1, 1)  # 定义几张图
+    fig.set_tight_layout(True)  # 紧凑布局
+    fig.set_size_inches(8, 4.5)  # 定义画布大小
+
+    axes.plot(x_value, y_value, 'o-', label="expectations")
+    axes.plot(x_value, y_sqrt_value, 'o-', label="sqrt_value")
+    axes.plot(x_value, difference_value, '-', label="difference_value")
+
+    plt.grid(ls='--', lw=0.01, color='black')  # 生成网格
+    # plt.legend(fontsize=20, loc='upper left')  # 标签字体大小
+    plt.legend(fontsize=20)  # 标签字体大小
+    plt.xticks(fontsize=15)  # x轴刻度字体大小
+    plt.yticks(fontsize=15)  # y轴刻度字体大小
+    plt.xlim(0, time_horizon+0.5)
+    # plt.ylim(0, 100)
+    plt.show()
+
